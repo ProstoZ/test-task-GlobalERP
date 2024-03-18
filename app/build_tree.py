1c32cca20b77502465e085675fed60ab8dc8b8f5
Alex <shalnev.aleksandr@vniizht-engineering.ru>
2024-03-18 19:41:44 +0300

    feature: add basic logic for tree building

---
+++ app/build_tree.py
@@ -0,0 +1,58 @@
+﻿from app.entities import TreeNode
+
+
+def parse_number(stack: list[TreeNode], num: str) -> str:
+    """
+    Добавляет число в стек как новый узел дерева, если число не пустое.
+
+    :param stack: Стек, содержащий узлы дерева и открывающие скобки.
+    :param num: Строка, содержащая число для преобразования в узел.
+    """
+    if num:
+        stack.append(TreeNode(int(num)))
+    return ""
+
+
+def handle_brackets(stack: list[TreeNode | str], char: str) -> None:
+    """
+    Обрабатывает скобки, формируя узлы дерева и их иерархию.
+
+    :param stack: Стек, содержащий узлы дерева и открывающие скобки.
+    :param char: Текущий обрабатываемый символ.
+    """
+    if char == "(":
+        stack.append(char)
+    elif char == ")":
+        nodes = []
+        while stack and stack[-1] != "(":
+            nodes.append(stack.pop())
+        stack.pop()
+        parent = stack.pop() if stack and isinstance(stack[-1], TreeNode) else None
+        for node in reversed(nodes):
+            if parent:
+                parent.add_child(node)
+            else:
+                parent = node
+        if parent:
+            stack.append(parent)
+
+
+def build_tree(input_string: str) -> TreeNode | None:
+    """
+    Строит дерево из строки c вложенными скобками.
+
+    :param input_string: Строка, содержащая числа и скобки.
+    :return: Корень построенного дерева или None, если дерево не может быть построено.
+    """
+    stack: list[TreeNode | str] = []
+    num: str = ""
+    for char in input_string:
+        if char.isdigit():
+            num += char
+        elif char in "()":
+            num = parse_number(stack, num)
+            handle_brackets(stack, char)
+        elif char == " " and num:
+            num = parse_number(stack, num)
+    parse_number(stack, num)
+    return stack[0] if stack else None
---
+++ app/entities.py
@@ -0,0 +1,38 @@
+﻿from app.constants import INDENT_LENGTH
+
+
+class TreeNode:
+    """
+    Класс, представляющий узел в структуре дерева.
+
+    Attributes:
+        - value (значение): Значение, хранящееся в узле.
+        - children (дети): Список объектов TreeNode, которые являются детьми этого узла.
+
+    Methods:
+        - add_child(self, child_node): Добавляет объект TreeNode в список детей.
+        - display_tree(self, level=0, prefix='', is_root=True): Рекурсивно выводит структуру дерева,
+            начиная с узла, на котором вызван этот метод. Дерево отображается с отступами,
+            представляющими глубину каждого узла, и визуальным представлением отношений родитель-ребенок.
+    """
+
+    def __init__(self, value: int) -> None:
+        self.value = value
+        self.children: list[TreeNode] = []
+
+    def add_child(self, child_node: "TreeNode") -> None:
+        self.children.append(child_node)
+
+    def display_tree(self, level: int = 0, last: bool = True, prefix: str = "") -> None:
+        indent = ""
+        connector = "-" * (INDENT_LENGTH - len(str(self.value))) + "+" if self.children else ""
+        if level == 0:
+            print(f"{self.value}{connector}")
+        else:
+            print(f"{prefix}{indent}{self.value}{connector}")
+
+        if self.children:
+            new_prefix = prefix + (" " * INDENT_LENGTH if last else "|" + " " * (INDENT_LENGTH - 1))
+            for i, child in enumerate(self.children):
+                is_last = i == len(self.children) - 1
+                child.display_tree(level + 1, is_last, new_prefix)
